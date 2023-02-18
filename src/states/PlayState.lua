daf97066df47783afb36a87b0a7bf02f1eee26be
CaraNetzer <cara.netzer@gmail.com>
2023-02-18 13:34:49 -0500

    plants growing

--- src/states/PlayState.lua
+++ src/states/PlayState.lua
@@ -28,6 +28,38 @@ function PlayState:init()
 
     self.player.currentAnimation = self.player.animations['idle-down']
 
+    self.waitDuration = 0
+    self.waitTimer = 0
+
+end
+
+function PlayState:growPlants(plant, dt)
+    if self.waitDuration == 0 then
+        self.waitDuration = math.random(5)
+    else
+        self.waitTimer = self.waitTimer + dt
+
+        if self.waitTimer > self.waitDuration then
+            -- if timer is up, change sprite to the next sapling in the set
+            if plant.sprite < 4 then
+                plant.sprite = plant.sprite + 1
+                self.waitTimer = 0
+                self.waitDuration = 0
+                print('ai less than 5: ' .. plant.sprite)
+            else -- if we're on the last sapling in the set and the timer is up, change to the plant and stay there
+                print('ai: ' .. plant.sprite)
+                plant.sprite = 6
+            end                
+        end
+    end
+end
+
+function PlayState:checkPlantOverlap(plants)
+    --for each plant 
+        --check collisions
+        --if a collision occurs
+            --remove plant from plants
+            --play error sound
 end
 
 function PlayState:update(dt)
@@ -40,54 +72,59 @@ function PlayState:update(dt)
     local mapWidth = (self.farm.size*TILE_SIZE*1.5)
     local mapHeight = (self.farm.size*TILE_SIZE)
     
-    if self.player.x < VIRTUAL_WIDTH/2 then
-        camera.x = 0
-    end
-    if self.player.y < VIRTUAL_HEIGHT/2 then
-        camera.y = 0    
-    end
-    if camera.x > mapWidth - mapWidth/2 then
-        camera.x = mapWidth - mapWidth/2
-    end
-    if camera.y > mapHeight - mapHeight/2 then
-        camera.y = mapHeight - mapHeight/2    
-    end
+    --camera movement
+    -- if self.player.x < VIRTUAL_WIDTH/2 then
+    --     camera.x = 0
+    -- end
+    -- if self.player.y < VIRTUAL_HEIGHT/2 then
+    --     camera.y = 0    
+    -- end
+    -- if camera.x > mapWidth - mapWidth/2 then
+    --     camera.x = mapWidth - mapWidth/2
+    -- end
+    -- if camera.y > mapHeight - mapHeight/2 then
+    --     camera.y = mapHeight - mapHeight/2    
+    -- end
     
-
-
-
 
     if love.keyboard.wasPressed('escape') then
         love.event.quit()
     end
 
+
+    --plant random seed based on player direction
     if love.keyboard.wasPressed('p') then
+        local plant = {
+            set = math.random(1,9),
+            sprite = 1,
+            x = self.player.x + 16,
+            y = self.player.y
+        }
+
         if self.player.direction == 'right' then
-            table.insert(self.plants, {
-                sprite = math.random(2, 35),
-                x = self.player.x + 16,
-                y = self.player.y
-            })
+            table.insert(self.plants, plant)
+            --checkPlantOverlap(self.plants)
         elseif self.player.direction == 'left' then
-            table.insert(self.plants, {
-                sprite = math.random(2, 35),
-                x = self.player.x - 25,
-                y = self.player.y
-            })
+            table.insert(self.plants, plant)
+            --checkPlantOverlap(self.plants)
         elseif self.player.direction == 'down' then
-            table.insert(self.plants, {
-                sprite = math.random(2, 35),
-                x = self.player.x - 5,
-                y = self.player.y + 16
-            })
+            table.insert(self.plants, plant)
+            --checkPlantOverlap(self.plants)
         elseif self.player.direction == 'up' then
-            table.insert(self.plants, {
-                sprite = math.random(2, 35),
-                x = self.player.x - 5,
-                y = self.player.y - 25
-            })
+            table.insert(self.plants, plant)
+            --checkPlantOverlap(self.plants)
         end
     end
+
+    --ai grow seeds
+    for i, plant in pairs(self.plants) do
+        self:growPlants(plant, dt)
+    end
+
+    --allow player to pick up fully grown plants
+    --if PLANT_TILES contains plant.sprite and player is next to a plant
+        --add plant to backpack
+        --remove plant from self.plants
 
     if love.keyboard.wasPressed('enter') or love.keyboard.wasPressed('return') then
         gStateMachine:change('start')
@@ -139,7 +176,15 @@ function PlayState:render()
     self.farm:render()
     
     for i, plant in pairs(self.plants) do
-        love.graphics.draw(gTextures['plants'], gFrames['plants'][SAPLING_TILES[plant.sprite]], plant.x, plant.y)
+        print('modulo: ' .. plant.sprite % 6)
+        print('sprite in render: ' .. plant.sprite)
+        if plant.sprite%6 ~= 0 then
+            print('sap: ' .. SAPLING_TILES[plant.set][plant.sprite])
+            love.graphics.draw(gTextures['plants'], gFrames['plants'][SAPLING_TILES[plant.set][plant.sprite]], plant.x, plant.y)
+        else
+            print('plant: ' .. PLANT_TILES[plant.sprite * plant.set])
+            love.graphics.draw(gTextures['plants'], gFrames['plants'][PLANT_TILES[plant.sprite * plant.set]], plant.x, plant.y)
+        end
     end
 
     if self.player.currentAnimation then
